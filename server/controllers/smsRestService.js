3445bcc0d8734a3b3651de3eb5cd888fda359e5c
nabiypy <nabiypy@gmail.com>
2018-05-16 11:16:02 +0000

    update logger info

--- server/controllers/smsRestService.js
+++ server/controllers/smsRestService.js
@@ -52,4 +52,47 @@ smsRestService.doPostSms = function (req, res) {
   });
 
 }
+smsRestService.getDeliveryReport = function (req, res) {
+  var body = req.body,
+    url = config.infoBib.baseUrl + config.infoBib.getDeliveryReport,
+    auth = "Basic " + new Buffer(config.infoBib.userName + ":" + config.infoBib.password).toString("base64");
+
+  logger.info('info', 'body request >>');
+  logger.info('info', body);
+
+  var payload = {
+    "from": req.body.from,
+    "to": req.body.to,
+    "text": req.body.message
+  };
+
+  var options = {
+    url: url,
+    json: true,
+    body: payload,
+    method: "POST",
+    headers: {
+      "Authorization": auth
+    }
+  };
+  console.log("options >>>", options);
+  logger.info("postSMS options == "+ options);
+  request(options, function (error, response, body) {
+    console.log('sms', response);
+    logger.info('send sms response >>>>');
+    if (error) {
+      console.log("error", "error sms >>> ", error);
+      logger.error("error sms >>> ", error);
+      res.json(error);
+    } else {
+      logger.log("infobip", "successful response >>");
+      console.log('result body >>>>', body);
+      logger.info('infobip request response body >>>>', body);
+
+      res.status(200).json(body);
+    }
+  });
+
+}
+
 module.exports = smsRestService;
