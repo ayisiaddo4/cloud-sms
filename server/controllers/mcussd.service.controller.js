553c3d0f59703269fa61f3747348a7ab151cc5ea
Hanson Peprah Yeboah <hanson@SASGHANA.COM>
2018-09-28 16:47:50 +0000

    demo dafabet

--- server/controllers/mcussd.service.controller.js
+++ server/controllers/mcussd.service.controller.js
@@ -38,51 +38,47 @@ MCUssd.ussdReceiver = function (req, callback) {
 } //get transaction status
 
 MCUssd.ussdCallBack = function (req, res) {
-  var body = req.body,
-      session_id=req.query.session_id,
-      type=req.query.type,
-      user_request=req.query.user_request,
-      phone=req.query.phone_number,
-      baseUrl =config.mc.baseUrl,
-      responseUrl =config.mc.clientResponseUrl,
-      ussdcode=config.mc.ussd_code,
-      message=config.mc.message,
-      url = baseUrl+responseUrl+"ussd_code"+"="+ussdcode+"&"+"response_message"+"="+message+"&"+"phone_number"+"="+phone;
+   var reqBody = req.body;
+    // console.log('incoming request body << >>>', reqBody);
+  var session_id = reqBody.session_id,
+    type = reqBody.type,
+    user_request = reqBody.user_request,
+    phone_number = reqBody.phone_number,
+    baseUrl = config.mc.baseUrl,
+    responseUrl = config.mc.clientResponseUrl,
+    ussdcode = config.mc.ussd_code,
+    message = reqBody.message,
+    url =baseUrl +responseUrl +"ussd_code" +"="+ussdcode+ "&"+"response_message"+"=" +message + "&" +"phone_number"+"="+phone_number;
 
-console.log('request body >>>',body);
+  console.log("==================================");
+  console.log("user request >>>", user_request);
+  console.log("Session Id >>>", session_id);
+  console.log("Type >>>", type);
+  console.log("mcUrl >>>" + url);
+  console.log("==================================");
 
-var options = {
-  url: url,
-  json: true,
-  method: "POST"
-};
-console.log("==================================");
-console.log("ussd_code >>>", ussdcode);
-console.log("response_message >>>", message);
-console.log("phone number >>>", phone);
-console.log("user request >>>", user_request);
-console.log("Session Id >>>", session_id);
-console.log("mcUrl >>>"+url);
-console.log("==================================");
+  var options = {
+    url: url,
+    json: true,
+    method: "POST"
+  };
+  logger.info("mc ussd callback options to post >>>", JSON.stringify(options));
+  request(options, function (error, response, body) {
+    // console.log(response);
+    if (error) {
+      console.log("error", "Error requesting MCUssd transaction status >>> ", error);
+      logger.info(error);
+      res.status(500).json(error);
+    } else {
 
-logger.info("Get transaction status", JSON.stringify(options));
-request(options, function (error, response, body) {
-  // console.log(response);
-  if (error) {
-    console.log("error", "Error requesting MCUssd transaction status >>> ", error);
-    logger.info(error);
-    res.status(500).json(error);
-  } else {
-    
-    // var durl = body, 
-    //     phone_number = durl.substr(18,25);
-    // console.log('phone from query string >>>>',phone_number);
-    console.log('response from ussd <<< ',body);
-    logger.info("initial callback from Service Provider" + JSON.stringify(body));
-    res.status(201).json(body);
-  }
-});
-
+      // var durl = body, 
+      //     phone_number = durl.substr(18,25);
+      // console.log('phone from query string >>>>',phone_number);
+      console.log('MC USSD gateway response >>> ', body);
+      logger.info("MC USSD client response message >>>" + JSON.stringify(body));
+      res.status(200).json(body);
+    }
+  });
 } //MCUssd callback responses
 
 
